06101734d9b8eb8ad619e145dbaf4f4aaa32122a
windowsagent <52929255+windowsagent@users.noreply.github.com>
2020-08-22 19:19:27 -0500

    Fixed it?

--- install.sh
+++ install.sh
@@ -1,20 +1,316 @@
+#!/usr/bin/env bash
+set -e
 
+# Arch Linux Install Script (alis) installs unattended, automated
+# and customized Arch Linux system.
+# Copyright (C) 2018 picodotdev
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+# This script is hosted at https://github.com/picodotdev/alis. For new features,
+# improvements and bugs fill an issue in GitHub or make a pull request.
+# Pull Request are welcome!
+#
+# If you test it in real hardware please send me an email to pico.dev@gmail.com with
+# the machine description and tell me if somethig goes wrong or all works fine.
+#
+# Please, don't ask for support for this script in Arch Linux forums, first read
+# the Arch Linux wiki [1], the Installation Guide [2] and the General
+# Recomendations [3], later compare the commands with those of this script.
+#
+# [1] https://wiki.archlinux.org
+# [2] https://wiki.archlinux.org/index.php/Installation_guide
+# [3] https://wiki.archlinux.org/index.php/General_recommendations
 
+# Usage:
+# # loadkeys es
+# # curl https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash, or with URL shortener curl -sL https://bit.ly/2F3CATp | bash
+# # vim alis.conf
+# # ./alis.sh
 
+# global variables (no configuration, don't edit)
+ASCIINEMA=""
+BIOS_TYPE=""
+PARTITION_BOOT=""
+PARTITION_ROOT=""
+PARTITION_BOOT_NUMBER=""
+PARTITION_ROOT_NUMBER=""
+DEVICE_ROOT=""
+DEVICE_LVM=""
+LUKS_DEVICE_NAME="cryptroot"
+LVM_VOLUME_GROUP="vg"
+LVM_VOLUME_LOGICAL="root"
+SWAPFILE="/swapfile"
+BOOT_DIRECTORY=""
+ESP_DIRECTORY=""
+#PARTITION_BOOT_NUMBER=0
+UUID_BOOT=""
+UUID_ROOT=""
+PARTUUID_BOOT=""
+PARTUUID_ROOT=""
+DEVICE_SATA=""
+DEVICE_NVME=""
+DEVICE_MMC=""
+CPU_VENDOR=""
+VIRTUALBOX=""
+CMDLINE_LINUX_ROOT=""
+CMDLINE_LINUX=""
 
+CONF_FILE="alis.conf"
+GLOBALS_FILE="alis-globals.conf"
+LOG_FILE="alis.log"
+ASCIINEMA_FILE="alis.asciinema"
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHT_BLUE='\033[1;34m'
+NC='\033[0m'
 
+function configuration_install() {
+    source alis.conf
+}
 
+function sanitize_variables() {
+    DEVICE=$(sanitize_variable "$DEVICE")
+    PARTITION_MODE=$(sanitize_variable "$PARTITION_MODE")
+    PARTITION_CUSTOMMANUAL_BOOT=$(sanitize_variable "$PARTITION_CUSTOMMANUAL_BOOT")
+    PARTITION_CUSTOMMANUAL_ROOT=$(sanitize_variable "$PARTITION_CUSTOMMANUAL_ROOT")
+    FILE_SYSTEM_TYPE=$(sanitize_variable "$FILE_SYSTEM_TYPE")
+    SWAP_SIZE=$(sanitize_variable "$SWAP_SIZE")
+    KERNELS=$(sanitize_variable "$KERNELS")
+    KERNELS_COMPRESSION=$(sanitize_variable "$KERNELS_COMPRESSION")
+    SYSTEMD_HOMED_STORAGE=$(sanitize_variable "$SYSTEMD_HOMED_STORAGE")
+    BOOTLOADER=$(sanitize_variable "$BOOTLOADER")
+    DESKTOP_ENVIRONMENT=$(sanitize_variable "$DESKTOP_ENVIRONMENT")
+    DISPLAY_DRIVER=$(sanitize_variable "$DISPLAY_DRIVER")
+    DISPLAY_DRIVER_HARDWARE_ACCELERATION_INTEL=$(sanitize_variable "$DISPLAY_DRIVER_HARDWARE_ACCELERATION_INTEL")
+    PACKAGES_PACMAN=$(sanitize_variable "$PACKAGES_PACMAN")
+    AUR=$(sanitize_variable "$AUR")
+    PACKAGES_AUR=$(sanitize_variable "$PACKAGES_AUR")
+}
 
+function sanitize_variable() {
+    VARIABLE=$1
+    VARIABLE=$(echo $VARIABLE | sed "s/![^ ]*//g") # remove disabled
+    VARIABLE=$(echo $VARIABLE | sed "s/ {2,}/ /g") # remove unnecessary white spaces
+    VARIABLE=$(echo $VARIABLE | sed 's/^[[:space:]]*//') # trim leading
+    VARIABLE=$(echo $VARIABLE | sed 's/[[:space:]]*$//') # trim trailing
+    echo "$VARIABLE"
+}
 
+function check_variables() {
+    check_variables_value "KEYS" "$KEYS"
+    check_variables_boolean "LOG" "$LOG"
+    check_variables_value "DEVICE" "$DEVICE"
+    check_variables_boolean "DEVICE_TRIM" "$DEVICE_TRIM"
+    check_variables_boolean "LVM" "$LVM"
+    check_variables_list "FILE_SYSTEM_TYPE" "$FILE_SYSTEM_TYPE" "ext4 btrfs xfs"
+    check_variables_equals "LUKS_PASSWORD" "LUKS_PASSWORD_RETYPE" "$LUKS_PASSWORD" "$LUKS_PASSWORD_RETYPE"
+    check_variables_list "PARTITION_MODE" "$PARTITION_MODE" "auto custom manual" "true"
+    if [ "$PARTITION_MODE" == "custom" ]; then
+        check_variables_value "PARTITION_CUSTOM_PARTED_UEFI" "$PARTITION_CUSTOM_PARTED_UEFI"
+        check_variables_value "PARTITION_CUSTOM_PARTED_BIOS" "$PARTITION_CUSTOM_PARTED_BIOS"
+    fi
+    if [ "$PARTITION_MODE" == "custom" -o "$PARTITION_MODE" == "manual" ]; then
+        check_variables_value "PARTITION_CUSTOMMANUAL_BOOT" "$PARTITION_CUSTOMMANUAL_BOOT"
+        check_variables_value "PARTITION_CUSTOMMANUAL_ROOT" "$PARTITION_CUSTOMMANUAL_ROOT"
+    fi
+    if [ "$LVM" == "true" ]; then
+        check_variables_list "PARTITION_MODE" "$PARTITION_MODE" "auto" "true"
+    fi
+    check_variables_value "PING_HOSTNAME" "$PING_HOSTNAME"
+    check_variables_value "PACMAN_MIRROR" "$PACMAN_MIRROR"
+    check_variables_list "KERNELS" "$KERNELS" "linux-lts linux-lts-headers linux-hardened linux-hardened-headers linux-zen linux-zen-headers" "false"
+    check_variables_list "KERNELS_COMPRESSION" "$KERNELS_COMPRESSION" "gzip bzip2 lzma xz lzop lz4" "false"
+    check_variables_value "TIMEZONE" "$TIMEZONE"
+    check_variables_boolean "REFLECTOR" "$REFLECTOR"
+    check_variables_value "PACMAN_MIRROR" "$PACMAN_MIRROR"
+    check_variables_value "LOCALES" "$LOCALES"
+    check_variables_value "LOCALE_CONF" "$LOCALE_CONF"
+    check_variables_value "LANG" "$LANG"
+    check_variables_value "KEYMAP" "$KEYMAP"
+    check_variables_value "HOSTNAME" "$HOSTNAME"
+    check_variables_value "USER_NAME" "$USER_NAME"
+    check_variables_value "USER_PASSWORD" "$USER_PASSWORD"
+    check_variables_equals "ROOT_PASSWORD" "ROOT_PASSWORD_RETYPE" "$ROOT_PASSWORD" "$ROOT_PASSWORD_RETYPE"
+    check_variables_equals "USER_PASSWORD" "USER_PASSWORD_RETYPE" "$USER_PASSWORD" "$USER_PASSWORD_RETYPE"
+    check_variables_boolean "SYSTEMD_HOMED" "$SYSTEMD_HOMED"
+    if [ "$SYSTEMD_HOMED" == "true" ]; then
+        check_variables_list "SYSTEMD_HOMED_STORAGE" "$SYSTEMD_HOMED_STORAGE" "directory fscrypt luks cifs subvolume" "true"
 
+        if [ "$SYSTEMD_HOMED_STORAGE" == "fscrypt" ]; then
+            check_variables_list "FILE_SYSTEM_TYPE" "$FILE_SYSTEM_TYPE" "ext4 f2fs" "true"
+        fi
+        if [ "$SYSTEMD_HOMED_STORAGE" == "cifs" ]; then
+            check_variables_value "SYSTEMD_HOMED_CIFS_DOMAIN" "$SYSTEMD_HOMED_CIFS_DOMAIN"
+            check_variables_value "SYSTEMD_HOMED_CIFS_SERVICE" "$SYSTEMD_HOMED_CIFS_SERVICE"
+        fi
+    fi
+    check_variables_value "HOOKS" "$HOOKS"
+    check_variables_list "BOOTLOADER" "$BOOTLOADER" "grub refind systemd"
+    check_variables_list "AUR" "$AUR" "aurman yay" "false"
+    check_variables_list "DESKTOP_ENVIRONMENT" "$DESKTOP_ENVIRONMENT" "gnome kde xfce mate cinnamon lxde" "false"
+    check_variables_list "DISPLAY_DRIVER" "$DISPLAY_DRIVER" "intel amdgpu ati nvidia nvidia-lts nvidia-dkms nvidia-390xx nvidia-390xx-lts nvidia-390xx-dkms nouveau" "false"
+    check_variables_boolean "KMS" "$KMS"
+    check_variables_boolean "DISPLAY_DRIVER_DDX" "$DISPLAY_DRIVER_DDX"
+    check_variables_boolean "DISPLAY_DRIVER_HARDWARE_ACCELERATION" "$DISPLAY_DRIVER_HARDWARE_ACCELERATION"
+    check_variables_list "DISPLAY_DRIVER_HARDWARE_ACCELERATION_INTEL" "$DISPLAY_DRIVER_HARDWARE_ACCELERATION_INTEL" "intel-media-driver libva-intel-driver" "false"
+    check_variables_boolean "REBOOT" "$REBOOT"
+}
 
+function check_variables_value() {
+    NAME=$1
+    VALUE=$2
+    if [ -z "$VALUE" ]; then
+        echo "$NAME environment variable must have a value."
+        exit
+    fi
+}
 
+function check_variables_boolean() {
+    NAME=$1
+    VALUE=$2
+    check_variables_list "$NAME" "$VALUE" "true false"
+}
 
+function check_variables_list() {
+    NAME=$1
+    VALUE=$2
+    VALUES=$3
+    REQUIRED=$4
+    if [ "$REQUIRED" == "" -o "$REQUIRED" == "true" ]; then
+        check_variables_value "$NAME" "$VALUE"
+    fi
 
+    if [ "$VALUE" != "" -a -z "$(echo "$VALUES" | grep -F -w "$VALUE")" ]; then
+        echo "$NAME environment variable value [$VALUE] must be in [$VALUES]."
+        exit
+    fi
+}
 
+function check_variables_equals() {
+    NAME1=$1
+    NAME2=$2
+    VALUE1=$3
+    VALUE2=$4
+    if [ "$VALUE1" != "$VALUE2" ]; then
+        echo "$NAME1 and $NAME2 must be equal [$VALUE1, $VALUE2]."
+        exit
+    fi
+}
+
+function check_variables_size() {
+    NAME=$1
+    SIZE_EXPECT=$2
+    SIZE=$3
+    if [ "$SIZE_EXPECT" != "$SIZE" ]; then
+        echo "$NAME array size [$SIZE] must be [$SIZE_EXPECT]."
+        exit
+    fi
+}
+
+function warning() {
+    echo -e "${LIGHT_BLUE}Welcome to Arch Linux Install Script${NC}"
+    echo ""
+    echo -e "${RED}Warning"'!'"${NC}"
+    echo -e "${RED}This script deletes all partitions of the persistent${NC}"
+    echo -e "${RED}storage and continuing all your data in it will be lost.${NC}"
+    echo ""
+    read -p "Do you want to continue? [y/N] " yn
+    case $yn in
+        [Yy]* )
+            ;;
+        [Nn]* )
+            exit
+            ;;
+        * )
+            exit
+            ;;
+    esac
+}
+
+function init() {
+    print_step "init()"
+
+    init_log
+    loadkeys $KEYS
+}
+
+function init_log() {
+    if [ "$LOG" == "true" ]; then
+        exec > >(tee -a $LOG_FILE)
+        exec 2> >(tee -a $LOG_FILE >&2)
+    fi
+    set -o xtrace
+}
+
+function facts() {
+    print_step "facts()"
+
+    if [ -d /sys/firmware/efi ]; then
+        BIOS_TYPE="uefi"
+    else
+        BIOS_TYPE="bios"
+    fi
+
+    if [ -f "$ASCIINEMA_FILE" ]; then
+        ASCIINEMA="true"
+    else
+        ASCIINEMA="false"
+    fi
+
+    DEVICE_SATA="false"
+    DEVICE_NVME="false"
+    DEVICE_MMC="false"
+    if [ -n "$(echo $DEVICE | grep "^/dev/[a-z]d[a-z]")" ]; then
+        DEVICE_SATA="true"
+    elif [ -n "$(echo $DEVICE | grep "^/dev/nvme")" ]; then
+        DEVICE_NVME="true"
+    elif [ -n "$(echo $DEVICE | grep "^/dev/mmc")" ]; then
+        DEVICE_MMC="true"
+    fi
+
+    if [ -n "$(lscpu | grep GenuineIntel)" ]; then
+        CPU_VENDOR="intel"
+    elif [ -n "$(lscpu | grep AuthenticAMD)" ]; then
+        CPU_VENDOR="amd"
+    fi
+
+    if [ -n "$(lspci | grep -i virtualbox)" ]; then
+        VIRTUALBOX="true"
+    fi
+}
+
+function check_facts() {
+    if [ "$BIOS_TYPE" == "bios" ]; then
+        check_variables_list "BOOTLOADER" "$BOOTLOADER" "grub"
+    fi
+}
+
+function prepare() {
+    print_step "prepare()"
+
+    configure_time
+    prepare_partition
+    configure_network
+
+    pacman -Sy
+}
+
+function configure_time() {
+    timedatectl set-ntp true
+}
     # mount
     if [ "$FILE_SYSTEM_TYPE" == "btrfs" ]; then
         mount -o "$PARTITION_OPTIONS" "$DEVICE_ROOT" /mnt
