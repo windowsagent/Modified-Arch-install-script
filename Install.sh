f067022184101f35de2e539acf67becc261b3e61
windowsagent <52929255+windowsagent@users.noreply.github.com>
2020-11-02 20:08:30 -0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,38 +1,33 @@
 #!/usr/bin/env bash
+mkfs.fat -F 32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mkswap /dev/sda3
 swapon /dev/sda3
 mount /dev/sda2 /mnt
+mkdir /mnt/boot
 timedatectl set-ntp true
-curl "https://raw.githubusercontent.com/windowsagent/Modified-Arch-install-script/master/mirrorlist" >> mirrorlist
-cp mirrorlist /etc/pacman.d/mirrorlist
-pacstrap /mnt base
-arch-chroot /mnt pacman -S --noconfirm --needed linux linux-firmware
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt ln -s -f /usr/share/zoneinfo/USA/Eastern /etc/localtime
-arch-chroot /mnt hwclock --systohc
-sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
+basestrap /mnt base base-devel openrc
+basestrap /mnt linux linux-firmware
+fstabgen -U /mnt >> /mnt/etc/fstab
+artools-chroot /mnt  ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
+artools-chroot /mnt hwclock --systohc
 sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /mnt/etc/locale.gen
-locale-gen
-arch-chroot /mnt locale-gen
+sed -i "s/#es_MX.UTF-8 UTF-8/es_MX.UTF-8 UTF-8/" /mnt/etc/locale.gen
+artools-chroot /mnt locale-gen
+artools-chroot /mnt pacman -S --noconfirm grub os-prober efibootmgr
+artools-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+artools-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 echo Insert the host name, please.
 read HOSTNAME
 echo Welcome to the world of Arch linux, $HOSTNAME!
 echo $HOSTNAME > /mnt/etc/hostname
-arch-chroot /mnt pacman -S --noconfirm --needed networkmanager
-arch-chroot /mnt systemctl enable NetworkManager.service
-create_user "windowsagent" "2006"
-printf "2006\n2006" | arch-chroot /mnt passwd
-arch-chroot /mnt pacman -S --noconfirm --needed sudo
-arch-chroot /mnt sed -i 'windowsagent ALL=(ALL)' /etc/sudoers
-arch-chroot /mnt pacman -S --noconfirm --needed grub
-arch-chroot /mnt grub-install --target=i386-pc --recheck /dev/sda
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-arch-chroot /mnt systemctl set-default graphical.target
-arch-chroot /mnt pacman -S --noconfirm --needed xfce4 xfce4-goodies lightdm lightdm-gtk-greeter xorg-server
-arch-chroot /mnt systemctl enable lightdm.service
-arch-chroot /mnt sed -i 's/%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
-arch-chroot /mnt pacman -S --noconfirm --needed git base-devel
+artools-chroot /mnt pacman -S --noconfirm --needed networkmanager
+artools-chroot /mnt systemctl enable NetworkManager.service
+artools-chroot /mnt pacman -S dhcpcd
+useradd -m windowsagent
+artools-chroot /mnt pacman -S --noconfirm --needed sudo
+artools-chroot /mnt sed -i 'windowsagent ALL=(ALL)' /etc/sudoers
+artools-chroot /mnt pacman -S nvidia
 # Run a script inside a chroot environment
 
 
@@ -54,15 +49,4 @@ echo " "
 echo "You can now proceed to reboot your system :3"
 echo "*computer* Huh, this was a whole journey!"
 echo " "
-function create_user() {
-    USER_NAME=$1
-    USER_PASSWORD=$2
-    if [ "$SYSTEMD_HOMED" == "true" ]; then
-        arch-chroot /mnt systemctl enable systemd-homed.service
-        create_user_homectl $USER_NAME $USER_PASSWORD
-#       create_user_useradd $USER_NAME $USER_PASSWORD
-    else
-        create_user_useradd $USER_NAME $USER_PASSWORD
-    fi
-}
 # This code is a mess, I know.
