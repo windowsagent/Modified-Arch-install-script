df1bbcee1cba951c17509064207e96669daedf80
windowsagent <52929255+windowsagent@users.noreply.github.com>
2021-07-20 21:27:07 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,39 +1,52 @@
 mkdir /mnt/boot
 timedatectl set-ntp true
-basestrap /mnt base base-devel openrc
-basestrap /mnt linux linux-firmware
-fstabgen -U /mnt >> /mnt/etc/fstab
-artools-chroot /mnt  ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
-artools-chroot /mnt hwclock --systohc
+reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+pacstrap /mnt base base-devel
+pacstrap /mnt linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt  ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
+arch-chroot /mnt hwclock --systohc
 sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /mnt/etc/locale.gen
 sed -i "s/#es_MX.UTF-8 UTF-8/es_MX.UTF-8 UTF-8/" /mnt/etc/locale.gen
-artools-chroot /mnt locale-gen
-artools-chroot /mnt pacman -S --noconfirm grub os-prober efibootmgr
-artools-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
-artools-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt locale-gen
+arch-chroot /mnt pacman -S --noconfirm grub os-prober efibootmgr nano
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 echo Insert the host name, please.
 read HOSTNAME
 echo Welcome to the world of Arch linux, $HOSTNAME!
 echo $HOSTNAME > /mnt/etc/hostname
-artools-chroot /mnt pacman -S --noconfirm --needed networkmanager
-artools-chroot /mnt systemctl enable NetworkManager.service
-artools-chroot /mnt pacman -S dhcpcd
+arch-chroot /mnt pacman -S --noconfirm --needed networkmanager
+arch-chroot /mnt systemctl enable NetworkManager.service
+arch-chroot /mnt pacman -S dhcpcd
 useradd -m windowsagent
-artools-chroot /mnt pacman -S --noconfirm --needed sudo
-artools-chroot /mnt sed -i 'windowsagent ALL=(ALL)' /etc/sudoers
+arch-chroot /mnt pacman -S --noconfirm --needed sudo git
+arch-chroot /mnt sed -i 'windowsagent ALL=(ALL)' /etc/sudoers
 # Run a script inside a chroot environment
 
 
-cat <<EOF > /mnt/root/yayinstall.sh
-git clone https://aur.archlinux.org/yay.git /home/windowsagent/
-chmod 777 /home/windowsagent/
+cat <<EOF > /mnt/root/post.sh
+mkdir /home/windowsagent
+pacman -S --noconfirm zip unzip wget curl
+git clone https://aur.archlinux.org/yay.git /home/windowsagent/yay
+chmod 777 /home/windowsagent/yay/
 cd /home/windowsagent/yay/
-makepkg -si
-rm -rf /home/windowsagent/yay/
+echo Run makepkg -si > /home/windowsagent/Important.txt
+cd /home/windowsagent/
+
+# 07/20/2021 -- Implemented new "experimental" patches
+# I wanna kill myself, this is hard!
+
+wget https://raw.githubusercontent.com/windowsagent/Modified-Arch-install-script/master/configs.zip
+unzip configs.zip
+pacman -S --noconfirm --needed xfce4 xfce4-goodies sddm
+systemctl enable sddm.service
+
 exit # to leave the chroot
 EOF
+arch-chroot /mnt chmod +x /root/post.sh
+arch-chroot /mnt /root/post.sh
 
-arch-chroot /mnt /root/yayinstall.sh
 
 # Exit out of the chroot enviroment
 echo " "
@@ -41,5 +54,6 @@ echo -e "${GREEN}Arch Linux installed successfully" ' ! ' "${NC}"
 echo " "
 echo "You can now proceed to reboot your system :3"
 echo "*computer* Huh, this was a whole journey!"
+echo "Do not forget to read important.txt, in your user home directory, future Knox!"
 echo " "
 # This code is a mess, I know.
