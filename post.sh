924f3825effad82edf964cb9c6185353e1af119c
windowsagent <52929255+windowsagent@users.noreply.github.com>
2021-07-22 16:48:25 -0500

    Added post installation script

---
+++ post.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Installing desktop environment
+sudo pacman -S --noconfirm --needed xfce4 xfce4-goodies lightdm lightdm-webkit2-greeter xorg 
+sudo systemctl enable lightdm
+sudo curl https://raw.githubusercontent.com/windowsagent/Modified-Arch-install-script/master/lightdm.conf > /etc/lightdm/lightdm.conf
+
+# Installing yay
+git clone https://aur.archlinux.org/yay.git /home/windowsagent/yay
+sudo chmod 777 /home/windowsagent/yay/
+cd /home/windowsagent/yay/
+makepkg -si
+cd /home/windowsagent/
+
+# Update DE config, theme and icons
+wget https://raw.githubusercontent.com/windowsagent/Modified-Arch-install-script/master/configs.zip -P /home/windowsagent/
+sudo unzip configs.zip
+
+# Install alsa and pavucontrol
+sudo pacman -S --noconfirm --needed pulseaudio-alsa pavucontrol
+
+# Install black arch repository and basic tools
+sudo wget -O - https://blackarch.org/strap.sh | sudo bash
+sudo pacman -Syy --noconfirm --needed blackarch-webapp blackarch-scanner
