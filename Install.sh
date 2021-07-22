6b744071114431ff9c855e67c3ec12bddc3cfb23
windowsagent <52929255+windowsagent@users.noreply.github.com>
2021-07-22 17:53:16 -0500

    Added rankmirrors

--- Install.sh
+++ Install.sh
@@ -4,6 +4,7 @@ echo Insert the host name, please.
 read HOSTNAME
 
 mkdir /mnt/boot
+pacman -Sy --noconfirm pacman-contrib
 curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 -
 pacman -Sy
 timedatectl set-ntp true
