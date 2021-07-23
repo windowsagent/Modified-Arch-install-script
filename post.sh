10a20d2a2e83055395f4708078431ffdea04fa76
windowsagent <52929255+windowsagent@users.noreply.github.com>
2021-07-22 20:24:10 -0500

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 # Installing desktop environment
 sudo pacman -S --noconfirm --needed xfce4 xfce4-goodies lightdm lightdm-webkit2-greeter xorg 
 sudo systemctl enable lightdm
 sudo sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/g" /etc/lightdm/lightdm.conf
+
 # Installing yay
 git clone https://aur.archlinux.org/yay.git /home/windowsagent/yay
 sudo chmod 777 /home/windowsagent/yay/
@@ -11,7 +13,7 @@ makepkg -si
 cd /home/windowsagent/
 
 # Update DE config, theme and icons
-wget https://raw.githubusercontent.com/windowsagent/Modified-Arch-install-script/master/configs.zip -P /home/windowsagent/
+wget https://download1082.mediafire.com/ij7wn7kg0qwg/br4thdqnjcb2494/configs.zip -P /home/windowsagent/
 sudo unzip configs.zip
 
 # Install alsa and pavucontrol
