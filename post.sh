2bf44208dd0a972ff85c54f6bd5d6017d40a038c
windowsagent <52929255+windowsagent@users.noreply.github.com>
2021-07-25 12:21:52 -0500

    Update automatic theme installation

--- post.sh
+++ post.sh
@@ -17,7 +17,15 @@ yay -Sy lightdm-slick-greeter
 # Update DE config, theme and icons
 wget https://github.com/windowsagent/Modified-Arch-install-script/raw/master/configs.zip -P /home/windowsagent/
 sudo unzip configs.zip
-
+sudo rm -rf configs.zip
 
 # Install alsa and pavucontrol
 sudo pacman -S --noconfirm --needed pulseaudio-alsa pavucontrol pulseaudio alsa-firmware
+
+# Install theme
+cd /usr/share/
+sudo wget https://raw.githubusercontent.com/windowsagent/Modified-Arch-install-script/master/share.zip -P /usr/share/
+sudo unzip share.zip
+
+sudo cp -R /usr/share/themes/IndigoMagic /home/windowsagent/.themes/
+sudo cp -R /usr/share/icons/sgi-elementary-xfce /home/windowsagent/.icons/
