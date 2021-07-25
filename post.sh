e4285c0f79e13af9fc34b7eb01d6d94fe61cb6df
windowsagent <52929255+windowsagent@users.noreply.github.com>
2021-07-25 13:16:17 -0500

    Update post.sh

--- post.sh
+++ post.sh
@@ -9,15 +9,16 @@ sudo chmod 777 /home/windowsagent/yay/
 cd /home/windowsagent/yay/
 makepkg -si
 cd /home/windowsagent/
+rm -rf /home/windowsagent/yay
 
 #Install greeter
 
 yay -Sy lightdm-slick-greeter
 
 # Update DE config, theme and icons
-wget https://github.com/windowsagent/Modified-Arch-install-script/raw/master/configs.zip -P /home/windowsagent/
-sudo unzip configs.zip
-sudo rm -rf configs.zip
+wget https://raw.githubusercontent.com/windowsagent/Modified-Arch-install-script/master/Xfce4-stuff.zip -P /home/windowsagent/
+sudo unzip Xfce4-stuff.zip
+sudo rm -rf Xfce4-stuff.zip
 
 # Install alsa and pavucontrol
 sudo pacman -S --noconfirm --needed pulseaudio-alsa pavucontrol pulseaudio alsa-firmware
