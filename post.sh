2218ff602293a9eef284ae2c50a1d500df63b03f
windowsagent <52929255+windowsagent@users.noreply.github.com>
2021-07-25 14:31:17 -0500

    Implement basic ocs-url installer

--- post.sh
+++ post.sh
@@ -23,12 +23,10 @@ sudo rm -rf Xfce4-stuff.zip
 # Install alsa and pavucontrol
 sudo pacman -S --noconfirm --needed pulseaudio-alsa pavucontrol pulseaudio alsa-firmware
 
-# Install theme
-cd /usr/share/
-sudo wget https://raw.githubusercontent.com/windowsagent/Modified-Arch-install-script/master/share.zip -P /usr/share/
-sudo unzip share.zip
-
-sudo cp -R /usr/share/themes/IndigoMagic /home/windowsagent/.themes/
-sudo cp -R /usr/share/icons/sgi-elementary-xfce /home/windowsagent/.icons/
-
+# Fix weird vblank issue
 xfconf-query -c xfwm4 -p /general/vblank_mode -s off
+
+# Install ocs-url (for themes)
+sudo pacman -S qt5-base qt5-svg qt5-declarative qt5-quickcontrols
+wget https://github.com/windowsagent/Modified-Arch-install-script/raw/master/ocs-url-3.1.0-1-x86_64.pkg.tar.xz -P /home/windowsagent
+sudo pacman -U /home/windowsagent/ocs-url-3.1.0-1-x86_64.pkg.tar.xz
